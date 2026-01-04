1d67e7b2000174ab79445a17e2c7219927da7096
berserkedman <berserk.abdurahmanov@gmail.com>
2026-01-04 14:28:01 +0500

    Delete src directory

--- src/components/ui/Badge.jsx
+++
@@ -1,9 +0,0 @@
-import './Badge.css'
-
-export const Badge = ({ children, variant = 'default' }) => {
-  return (
-    <span className={`badge badge--${variant}`}>
-      {children}
-    </span>
-  )
-}
--- src/components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-import './Card.css'
-
-export const Card = ({ children, hover = false, ...props }) => {
-  return (
-    <div className={`card ${hover ? 'card--hover' : ''}`} {...props}>
-      {children}
-    </div>
-  )
-}
